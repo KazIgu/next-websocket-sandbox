bcfa46ca561ba5c2846da2971b47755def2db15b
KazIgu <kaz.igu.21@gmail.com>
2021-07-04 23:00:53 +0900

    add gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5K76X93' });
+  }, []);
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
