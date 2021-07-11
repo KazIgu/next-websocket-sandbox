8ec8be08244bfd2c151092dcdccd66574dc27284
KazIgu <kaz.igu.21@gmail.com>
2021-07-11 15:46:49 +0900

    add prisma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'next-auth/client';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import TagManager from 'react-gtm-module';
@@ -7,7 +8,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-5K76X93' });
   }, []);
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...pageProps}
+      />
+    </Provider>
+  );
 }
 export default MyApp;
